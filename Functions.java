9299a572e156171d24c6b0085ed3aaad92b2442c
Roman Spanek <roman.spanek@tul.cz>
2026-02-14 17:54:56 +0100

    Improve validation logic

--- Functions.java
+++ Functions.java
@@ -1,16 +1,15 @@
-
 class Functions {
-private final int LOWEST = 10;
-private final int HIGHEST = 100;
 
-	int add (int a, int b) {
-		if (a <  LOWEST || b > HIGHEST ){
-			System.out.println("Value a is lower then a threshold or value b is large then threshholt");
-			return 0;
-		}
-		return a + b;
-	}
+    private final int LOWEST = 10;
+    private final int HIGHEST = 100;
+
+    int add(int a, int b) {
+        if (a < LOWEST || a > HIGHEST ||
+            b < LOWEST || b > HIGHEST) {
+
+            System.out.println("Values must be between 10 and 100");
+            return -1;
+        }
+        return a + b;
+    }
 }
-
-
-
