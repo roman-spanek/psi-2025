8d07cedd3e706b04c4330a93e06be0fe669c5567
Roman Spanek <roman.spanek@tul.cz>
2026-02-14 17:51:01 +0100

    Refactor validation to use exception

--- Functions.java
+++ Functions.java
@@ -4,11 +4,10 @@ class Functions {
     private final int HIGHEST = 100;
 
     int add(int a, int b) {
-        if (a < LOWEST || a > HIGHEST ||
-            b < LOWEST || b > HIGHEST) {
-
-            System.out.println("Values must be between 10 and 100");
-            return -1;
+       if (a < LOWEST || b > HIGHEST) {
+            throw new IllegalArgumentException(
+                "Input values are outside allowed range."
+            );
         }
         return a + b;
     }
